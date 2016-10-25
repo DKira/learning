e4aed39b7d3dc79626cd8e78c43e3187810d31a6
DKira <523310516@qq.com>
2016-10-25 18:13:05 +0800

    Update Demo4ApplicationTests.java

--- temp/Demo4ApplicationTests.java
+++ temp/Demo4ApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
+//http://somefuture.iteye.com/blog/2247207
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Demo4Application.class)
 @WebAppConfiguration
